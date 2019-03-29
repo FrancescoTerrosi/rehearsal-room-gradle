84dde9278d77e44cc60b44a8a69db309fc718f41
Francesco Terrosi <francesco.terrosi1@gmail.com>
2019-03-29 17:27:38 +0100

    first commit

---
+++ rehearsal-room-gradle/src/main/java/org/unifi/ft/RehearsalRoomGradleApplication.java
@@ -0,0 +1,13 @@
+package org.unifi.ft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RehearsalRoomGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RehearsalRoomGradleApplication.class, args);
+	}
+
+}
