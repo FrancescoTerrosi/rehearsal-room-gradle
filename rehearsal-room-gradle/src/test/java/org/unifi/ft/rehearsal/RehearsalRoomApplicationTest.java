622ba73294ce589416b38454db3bd3dc8f424740
Francesco Terrosi <francesco.terrosi1@gmail.com>
2019-04-05 16:20:56 +0200

    Added app test

---
+++ rehearsal-room-gradle/src/test/java/org/unifi/ft/rehearsal/RehearsalRoomApplicationTest.java
@@ -0,0 +1,16 @@
+package org.unifi.ft.rehearsal;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RehearsalRoomApplicationTest {
+
+	@Test
+	public void contextLoads() {
+		assertTrue(true);
+	}
+
+}
